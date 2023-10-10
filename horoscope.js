e113e9d5ce0e4a9837b36085e2d21a0bdf52632b
mathesoncec <charles.carlin@rocketmail.com>
2023-10-10 10:59:11 +0100

    added astroMessages

--- horoscope.js
+++ horoscope.js
@@ -3,5 +3,21 @@
 const zodiacSigns = [
     "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo",
     "Libra", "Scorpio", "Sagittarius", "Capricorn", 'Aquarius', "Pisces"
-]
+];
 
+const astroMessages = [
+    "Today is a day to take risks and embrace change.",
+    "Your energy levels will be high, and you'll accomplish your goals.",
+    "A surprise is waiting for you today. Stay open to new opportunities.",
+    "Focus on self-care and relaxation to maintain your balance.",
+    "You'll have the courage to speak your mind and make a difference.",
+    "A creative project will lead to success. Trust your instincts.",
+    "Harmony and balance will be your guiding principles today.",
+    "Your determination will lead you to victory in your endeavors.",
+    "Adventure awaits! Be open to new experiences and ideas.",
+    "Your disciplined approach will help you achieve your goals.",
+    "Your innovative thinking will set you apart from the crowd.",
+    "Embrace your intuition; it will lead you in the right direction."
+];
+
+//
