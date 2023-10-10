dccbe98e7818edea47a1b0ba978c799d2abc5d2a
mathesoncec <charles.carlin@rocketmail.com>
2023-10-10 10:56:29 +0100

    added zodiac signs

---
+++ horoscope.js
@@ -0,0 +1,7 @@
+//define arrays of data for each part of the message
+
+const zodiacSigns = [
+    "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo",
+    "Libra", "Scorpio", "Sagittarius", "Capricorn", 'Aquarius', "Pisces"
+]
+
